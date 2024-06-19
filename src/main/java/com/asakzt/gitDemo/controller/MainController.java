1642f723c50accda5769452f99acd84372af1b44
Комаров Павел Анатольевич <komarovpa1@sovcombank.ru>
2024-06-19 14:34:58 +0700

    added userPage method

--- src/main/java/com/asakzt/gitDemo/controller/MainController.java
+++ src/main/java/com/asakzt/gitDemo/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.asakzt.gitDemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,15 @@ public class MainController {
     public String mainPage() {
         return "Hello World";
     }
+
+    @GetMapping("/{user}")
+    public String userPage(@PathVariable("user") String user) {
+        return String.format("Hello $s!!!!!!!!!!", user);
+    }
+
+    @GetMapping("/timeNow")
+    public String userPage(@PathVariable("user") String user) {
+        return String.format("Time : $s", 12345);
+    }
+
 }
